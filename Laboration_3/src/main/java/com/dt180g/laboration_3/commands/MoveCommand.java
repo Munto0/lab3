4d44120a7baa94a0a880de52429a761eaaf1b328
Munto0 <102483713+Munto0@users.noreply.github.com>
2025-06-09 21:10:17 +0200

    Update MoveCommand.java

--- Laboration_3/src/main/java/com/dt180g/laboration_3/commands/MoveCommand.java
+++ Laboration_3/src/main/java/com/dt180g/laboration_3/commands/MoveCommand.java
@@ -1,10 +1,7 @@
 package com.dt180g.laboration_3.commands;
-
 import com.dt180g.laboration_3.support.AppConfig;
 import com.dt180g.laboration_3.receiver.HanoiEngine;
 import com.dt180g.laboration_3.support.HanoiLogger;
-
-
 /**
  * MoveCommand encapsulates a single move action in the Towers of Hanoi game.
  * On execution, it moves a disc from the source tower to the destination tower
@@ -12,25 +9,20 @@ import com.dt180g.laboration_3.support.HanoiLogger;
  *
  * @author Muntaser Ibrahim
  */
-
 public class MoveCommand implements CommandInterface {
     private final int src, dst;
-
     /**
      * Constructs a Move from  source and destination towers.
      *
      * @param src the index of the tower to move a disc from
      * @param dst the index of the tower to move a disc to
      */
-
     public MoveCommand(int src, int dst) {
         this.src = src; this.dst = dst;
     }
-
     /**
      * performs the disc transfer in HanoiEngine and logs the move.
      */
-
     @Override
     public void execute() {
         HanoiEngine.INSTANCE.performMove(src, dst, true);
