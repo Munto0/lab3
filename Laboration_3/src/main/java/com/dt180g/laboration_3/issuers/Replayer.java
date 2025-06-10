6409f982ef78c2627fe999dc91068228c92b2554
Munto0 <102483713+Munto0@users.noreply.github.com>
2025-06-10 23:55:56 +0200

    Update Replayer.java

--- Laboration_3/src/main/java/com/dt180g/laboration_3/issuers/Replayer.java
+++ Laboration_3/src/main/java/com/dt180g/laboration_3/issuers/Replayer.java
@@ -1,19 +1,15 @@
 package com.dt180g.laboration_3.issuers;
-
 import com.dt180g.laboration_3.commands.MoveCommand;
 import com.dt180g.laboration_3.commands.NewGameCommand;
 import com.dt180g.laboration_3.commands.ShowCommand;
 import com.dt180g.laboration_3.invoker.CommandManager;
 import com.dt180g.laboration_3.support.AppConfig;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
-
 public class Replayer {
     private final BufferedReader reader;
-
     /**
      * Default constructor reads from the log file path.
      * @throws IOException if reading fails
@@ -24,14 +20,12 @@ public class Replayer {
     public Replayer() throws IOException, URISyntaxException {
         reader = new BufferedReader(new FileReader(AppConfig.getLogFilePath()));
     }
-
     /**
      * Test constructor that takes any BufferedReader.
      */
     public Replayer(BufferedReader reader) {
         this.reader = reader;
     }
-
     /**
      * Runs the replay: first line is disc count, then sequences of moves or undos.
      * @throws IOException if reading from the reader fails
@@ -41,7 +35,6 @@ public class Replayer {
         String line = reader.readLine();
         int discs = Integer.parseInt(line.trim());
         CommandManager.INSTANCE.executeCommand(new NewGameCommand(discs));
-
         // Replay each subsequent line
         while ((line = reader.readLine()) != null) {
             if (line.equals(AppConfig.LOG_UNDO_SYMBOL)) {
