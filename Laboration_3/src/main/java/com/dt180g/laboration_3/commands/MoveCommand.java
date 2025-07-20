840168da28cef9b24b3362704ea4ccb68d8623c8
Munto0 <102483713+Munto0@users.noreply.github.com>
2025-07-20 20:55:46 +0200

    Update MoveCommand.java

--- Laboration_3/src/main/java/com/dt180g/laboration_3/commands/MoveCommand.java
+++ Laboration_3/src/main/java/com/dt180g/laboration_3/commands/MoveCommand.java
@@ -1,7 +1,9 @@
 package com.dt180g.laboration_3.commands;
+
 import com.dt180g.laboration_3.support.AppConfig;
 import com.dt180g.laboration_3.receiver.HanoiEngine;
 import com.dt180g.laboration_3.support.HanoiLogger;
+
 /**
  * MoveCommand encapsulates a single move action in the Towers of Hanoi game.
  * On execution, it moves a disc from the source tower to the destination tower
@@ -11,8 +13,9 @@ import com.dt180g.laboration_3.support.HanoiLogger;
  */
 public class MoveCommand implements CommandInterface {
     private final int src, dst;
+
     /**
-     * Constructs a Move from  source and destination towers.
+     * Constructs a Move from source and destination towers.
      *
      * @param src the index of the tower to move a disc from
      * @param dst the index of the tower to move a disc to
@@ -20,6 +23,7 @@ public class MoveCommand implements CommandInterface {
     public MoveCommand(int src, int dst) {
         this.src = src; this.dst = dst;
     }
+
     /**
      * performs the disc transfer in HanoiEngine and logs the move.
      */
@@ -28,6 +32,8 @@ public class MoveCommand implements CommandInterface {
         HanoiEngine.INSTANCE.performMove(src, dst, true);
         HanoiLogger.getInstance().logInfo(src + " " + dst);
     }
+
+    /** Reverses the move and logs the undo symbol. */
     public void unExecute() {
         HanoiEngine.INSTANCE.performMove(dst, src, false);
         HanoiLogger.getInstance().logInfo(AppConfig.LOG_UNDO_SYMBOL);
