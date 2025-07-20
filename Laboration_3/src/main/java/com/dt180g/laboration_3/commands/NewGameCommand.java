bac0ff7e6176e4751f4d8b1a5a4054db99f0bec1
Munto0 <102483713+Munto0@users.noreply.github.com>
2025-07-20 20:56:12 +0200

    Update NewGameCommand.java

--- Laboration_3/src/main/java/com/dt180g/laboration_3/commands/NewGameCommand.java
+++ Laboration_3/src/main/java/com/dt180g/laboration_3/commands/NewGameCommand.java
@@ -1,27 +1,30 @@
 package com.dt180g.laboration_3.commands;
+
 import com.dt180g.laboration_3.receiver.HanoiEngine;
 import com.dt180g.laboration_3.support.HanoiLogger;
 import com.dt180g.laboration_3.support.AppConfig;
+
 /**
  * NewGameCommand initializes or restarts a Towers of Hanoi game with a specified number of discs.
- * It ensures the disc count is within the  limits, resets
- *  and logs the initial count.
+ * It ensures the disc count is within the limits, resets the engine and logger, and logs the initial count.
  *
  * @author Muntaser Ibrahim
  */
 public class NewGameCommand implements CommandInterface {
     private final int discs;
+
     /**
-     * Constructs a NewGameCommand with the  disc count.
+     * Constructs a NewGameCommand with the disc count.
      *
      * @param discs the desired number of discs to start the game
      */
     public NewGameCommand(int discs) {
         this.discs = discs;
     }
+
     /**
-     * Executes the new game command , resetting the game engine, resetting the logger
-     * and logging the starting disc count.
+     * Executes the new game command: resets the engine, resets the logger,
+     * and logs the starting disc count.
      */
     @Override
     public void execute() {
